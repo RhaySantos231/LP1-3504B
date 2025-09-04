9d7a5769cc35eff8a000cd1dff46ab70173468d1
Rhayssa Santos <professorarhay.bms@gmail.com>
2025-09-04 15:04:03 -0300

    Primeiro Commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,27 @@
+package org.example
+
+import java.sql.DriverManager.println
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+   /*
+   * Use val para variáveis imutáveis (constantes).
+   * Use var para variáveis mutáveis (que podem mudar de valor).
+   *
+   *
+   * Estrutura:
+   *  (=) Atribuição
+   *  var/val nomeDaVariavel = valor
+   * */
+
+    var nome = "Rhayssa"
+    val CPF = "00000000x"
+    var idade = 15
+    var peso = 50.0
+    //println mostra uma mensagem para o usuario
+    //print line
+    println("Olá Mundo!!")
+
+
+}
